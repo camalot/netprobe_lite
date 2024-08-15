0ce719c541493f54d3d8d3aa0df150fe1afae097
Ryan Conrad <camalot@gmail.com>
2024-08-14 20:06:22 -0500

    cleanup lintting

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
 if [[ "${NP_MODULE^^}" == "NETPROBE" ]]; then
-  python3 netprobe.py
-# ??? There is no collector.py in the repo
-# elif [[ "${MODULE}" == "COLLECTOR" ]]; then 
-#   python3 collector.py
+    python3 netprobe.py
 elif [[ "${NP_MODULE^^}" == "PRESENTATION" ]]; then 
-  python3 presentation.py
+    python3 presentation.py
 elif [[ "${NP_MODULE^^}" == "SPEEDTEST" ]]; then 
-  python3 netprobe_speedtest.py; 
+    python3 netprobe_speedtest.py; 
 elif [[ "${NP_MODULE^^}" == "FULL" ]]; then
-  python3 /app/main.py
+    python3 /app/main.py
 else 
-  /bin/bash
+    /bin/bash
 fi
