beab35e40b8303a8a69724496d8a0eee223bbe22
Ryan Conrad <camalot@gmail.com>
2024-08-14 20:17:03 -0500

    fixed indent for sh file

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
 
 if [[ "${NP_MODULE^^}" == "NETPROBE" ]]; then
-  -  python3 netprobe.py
-  +	python3 netprobe.py
-   elif [[ "${NP_MODULE^^}" == "PRESENTATION" ]]; then
-  -  python3 presentation.py
-  +	python3 presentation.py
-   elif [[ "${NP_MODULE^^}" == "SPEEDTEST" ]]; then
-  -  python3 netprobe_speedtest.py
-  +	python3 netprobe_speedtest.py
-   elif [[ "${NP_MODULE^^}" == "FULL" ]]; then
-  -  python3 /app/main.py
-  +	python3 /app/main.py
-   else
-  -  /bin/bash
-  +	/bin/bash
+	python3 netprobe.py
+elif [[ "${NP_MODULE^^}" == "PRESENTATION" ]]; then
+	python3 presentation.py
+elif [[ "${NP_MODULE^^}" == "SPEEDTEST" ]]; then
+	python3 netprobe_speedtest.py
+elif [[ "${NP_MODULE^^}" == "FULL" ]]; then
+	python3 /app/main.py
+else
+	/bin/bash
 fi
