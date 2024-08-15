65b7a8b75c62cd5a6cd6d5de75cd4fa75f039b28
Ryan Conrad <camalot@gmail.com>
2024-08-14 20:11:15 -0500

    cleanup lintting

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 if [[ "${NP_MODULE^^}" == "NETPROBE" ]]; then
-    python3 netprobe.py
-elif [[ "${NP_MODULE^^}" == "PRESENTATION" ]]; then 
-    python3 presentation.py
-elif [[ "${NP_MODULE^^}" == "SPEEDTEST" ]]; then 
-    python3 netprobe_speedtest.py; 
+  python3 netprobe.py
+elif [[ "${NP_MODULE^^}" == "PRESENTATION" ]]; then
+  python3 presentation.py
+elif [[ "${NP_MODULE^^}" == "SPEEDTEST" ]]; then
+  python3 netprobe_speedtest.py
 elif [[ "${NP_MODULE^^}" == "FULL" ]]; then
-    python3 /app/main.py
-else 
-    /bin/bash
+  python3 /app/main.py
+else
+  /bin/bash
 fi
