cd05ca2c5b3d5b8dc17e59f0067051e20b61732e
Ryan Conrad <camalot@gmail.com>
2024-08-18 17:54:46 -0500

    remove some env vars that arent needed.

--- src/netprobe.py
+++ src/netprobe.py
@@ -10,19 +10,19 @@ from helpers.redis import RedisConnect
 
 class Netprobe:
     def __init__(self):
-        self.probe_interval = Config_Netprobe.probe_interval
-        probe_count = Config_Netprobe.probe_count
-        sites = Config_Netprobe.sites
-        dns_test_site = Config_Netprobe.dns_test_site
-        nameservers = Config_Netprobe.nameservers
-        self.device_id = Config_Netprobe.device_id
+        self.probe_interval = NetprobeConifguration.probe_interval
+        probe_count = NetprobeConifguration.probe_count
+        sites = NetprobeConifguration.sites
+        dns_test_site = NetprobeConifguration.dns_test_site
+        nameservers = NetprobeConifguration.nameservers
+        self.device_id = NetprobeConifguration.device_id
         self.collector = NetworkCollector(sites, probe_count, dns_test_site, nameservers)
 
         # Logging Config
         self.logger = setup_logging()
 
         # Logging each nameserver
-        for nameserver, ip, type in Config_Netprobe.nameservers:
+        for nameserver, ip, type in NetprobeConifguration.nameservers:
             self.logger.info(f"NAMESERVER: {nameserver} IP: {ip} TYPE: {type}")
 
     def run(self):
