2b23c03ae2a87d830c2ae9538e497afc6b556e45
Ryan Conrad <camalot@gmail.com>
2024-08-18 13:14:15 -0500

    set log colors

--- src/helpers/logging_helper.py
+++ src/helpers/logging_helper.py
@@ -8,18 +8,46 @@ from logging.handlers import RotatingFileHandler
 class ColorFormatter(logging.Formatter):
     def __init__(self, fmt=None, datefmt=None, validate=True):
         super().__init__(fmt=fmt, datefmt=datefmt, validate=validate)
-        self.reset = "\033[0m"
-        self.log_colors = {
-            logging.CRITICAL: "\033[91m",
-            logging.ERROR: "\033[91m",
-            logging.WARNING: "\033[93m",
-            logging.INFO: "\033[92m",
-            logging.DEBUG: "\033[94m",
+        self.colors = {
+            "RESET": "\u001b[0m",
+
+            "FGBLACK": "\u001b[30m",
+            "FGRED": "\u001b[31m",
+            "FGGREEN": "\u001b[32m",
+            "FGYELLOW": "\u001b[33m",
+            "FGBLUE": "\u001b[34m",
+            "FGMAGENTA": "\u001b[35m",
+            "FGCYAN": "\u001b[36m",
+            "FGWHITE": "\u001b[37m",
+
+            "BGBLACK": "\u001b[40m",
+            "BGRED": "\u001b[41m",
+            "BGGREEN": "\u001b[42m",
+            "BGYELLOW": "\u001b[43m",
+            "BGBLUE": "\u001b[44m",
+            "BGMAGENTA": "\u001b[45m",
+            "BGCYAN": "\u001b[46m",
+            "BGWHITE": "\u001b[47m",
+
+            "BOLD": "\u001b[1m",
+            "UNDERLINE": "\u001b[4m",
+            "REVERSE": "\u001b[7m",
         }
 
     def format(self, record):
-        log_color = self.log_colors.get(record.levelno, self.reset)
-        record.msg = f"{log_color}{record.msg}{self.reset}"
+        if record.levelno == logging.DEBUG:
+            log_color = f'{self.colors["FGBLUE"]}'
+        elif record.levelno == logging.INFO:
+            log_color = f'{self.colors["FGGREEN"]}'
+        elif record.levelno == logging.WARNING:
+            log_color = f'{self.colors["FGYELLOW"]}'
+        elif record.levelno == logging.ERROR:
+            log_color = f'{self.colors["FGRED"]}'
+        elif record.levelno == logging.CRITICAL:
+            log_color = f'{self.colors["BOLD"]}{self.colors["FGWHITE"]}{self.colors["BGRED"]}'
+        else:
+            log_color = self.colors["RESET"]
+        record.msg = f"{self.colors['RESET']}{log_color}{record.msg}{self.colors['RESET']}"
         return super().format(record)
 
 
